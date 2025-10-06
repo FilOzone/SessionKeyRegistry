75251e9668b4e52f5265697e429517794119fcff
William Morriss <wjmelements@gmail.com>
2025-10-06 15:13:29 -0500

    forge fmt

--- src/SessionKeyRegistry.sol
+++ src/SessionKeyRegistry.sol
@@ -1,17 +1,12 @@
 // SPDX-License-Identifier: Apache-2.0 OR MIT
-pragma solidity^0.8.30;
+pragma solidity ^0.8.30;
 
 contract SessionKeyRegistry {
-    mapping (
-        address user => mapping (
-            address signer => mapping (
-                bytes32 permission => uint256
-            )
-        )
-    ) public authorizationExpiry;
+    mapping(address user => mapping(address signer => mapping(bytes32 permission => uint256))) public
+        authorizationExpiry;
 
     function _setAuthorizations(address signer, uint256 expiry, bytes32[] calldata permissions) internal {
-        mapping (bytes32 => uint256) storage permissionExpiry = authorizationExpiry[msg.sender][signer];
+        mapping(bytes32 => uint256) storage permissionExpiry = authorizationExpiry[msg.sender][signer];
         for (uint256 i = 0; i < permissions.length; i++) {
             permissionExpiry[permissions[i]] = expiry;
         }
--- test/SessionKeyRegistry.t.sol
+++ test/SessionKeyRegistry.t.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: Apache-2.0 OR MIT
-pragma solidity^0.8.30;
+pragma solidity ^0.8.30;
 
 import {Test} from "forge-std/Test.sol";
 import {SessionKeyRegistry} from "../src/SessionKeyRegistry.sol";
